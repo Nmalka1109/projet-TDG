9f8749a43a8034754a7186fb95d66fc413b7ec8b
1801801 <louis.roussel@edu.ece.fr>
2018-04-06 10:20:15 +0200

    premier menu

---
+++ menu/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+//#include <allegro.h>
+#include <time.h>
+//#include <png.h>
+//#include <loadpng.h>
+//#include <jpgalleg.h>
+
+/*///test chargement image
+BITMAP * load_bitmap_check(char *nomImage)
+{
+    BITMAP *bmp;
+    bmp=load_bitmap(nomImage,NULL);
+    if (!bmp)
+    {
+        allegro_message("pas pu trouver %s",nomImage);
+        exit(EXIT_FAILURE);
+    }
+    return bmp;
+}*/
+
+
+int main()
+{
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
